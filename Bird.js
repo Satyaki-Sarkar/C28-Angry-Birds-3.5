71954c2b37b9e430131dfd978dcc7edb6e906988
Satyaki <satyakisarkar7500@gmail.com>
2020-09-02 18:08:21 +0530

    Angry Birds with slingshot

--- Bird.js
+++ Bird.js
@@ -2,11 +2,12 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.body.density=1.5;
   }
 
   display() {
-    this.body.position.x = mouseX;
-    this.body.position.y = mouseY;
+    // this.body.position.x = mouseX;
+    // this.body.position.y = mouseY;
     super.display();
   }
 }
